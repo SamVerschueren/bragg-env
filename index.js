54a2da957999b6318b40940d3159924444b995a2
Sam Verschueren <sam.verschueren@gmail.com>
2018-01-29 11:02:11 +0100

    Tweaks

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict';
 const environment = require('aws-lambda-env');
+
 module.exports = opts => {
 	return ctx => {
 		Object.defineProperty(ctx, 'env', {enumerable: true, value: process.env.NODE_ENV || environment(ctx.context, opts)});
